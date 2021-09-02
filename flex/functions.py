387f1caef8b3b70fa8da9df8a5496adaa18666ae
tomasz.pionka <tomasz.pionka@kinesso.com>
2021-09-02 14:59:33 +0200

    Changed log delimiter

--- flex/functions.py
+++ flex/functions.py
@@ -4,7 +4,7 @@ import requests
 
 def success_log(get_app_id, get_report_type, code, message):
     with open('success.txt', 'a+') as file_object:
-        log = '{}_{}_{}_{}_{}'.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), get_app_id, get_report_type,
+        log = '{}, {}, {}, {}, {}'.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), get_app_id, get_report_type,
                                       code, message)
         file_object.seek(0)
         logs = file_object.read(100)
@@ -17,7 +17,7 @@ def success_log(get_app_id, get_report_type, code, message):
 
 def error_log(get_app_id, get_report_type, error_code, message):
     with open('error.txt', 'a+') as file_object:
-        log = '{}_{}_{}_{}_{}'.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), get_app_id, get_report_type,
+        log = '{}, {}, {}, {}, {}'.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), get_app_id, get_report_type,
                                       error_code, message)
         file_object.seek(0)
         logs = file_object.read(100)
