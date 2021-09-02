10935d5a588ad6164766618501c738c7da0025a4
tomasz.pionka <tomasz.pionka@kinesso.com>
2021-09-02 13:13:14 +0200

    Added request function

--- flex/flex_android_installs.py
+++ flex/flex_android_installs.py
@@ -1,5 +1,6 @@
 import json
 from datetime import date, timedelta
+import requests
 
 with open('credentials.json') as file:
     credentials = json.load(file)
@@ -17,3 +18,20 @@ params = {
 
 url = 'https://hq.appsflyer.com/export/{}/{}/v5?api_token={}{}'.format(app_id, report_type, params['api_token'],
                                                                        params['sfx'])
+
+
+def get_stream(get_url, get_params):
+    response = requests.request('GET', url=get_url, params=get_params)
+    if response.status_code != 200:
+        if response.status_code == 400:
+            # TODO error log
+            print("")
+        else:
+            # TODO error log
+            print("")
+    else:
+        stream = response.text
+        return stream.split('\n')[1:-1]
+
+
+data = get_stream(url, params)
