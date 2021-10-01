790ba34155ad64f0f80a964762ebd4c05ede376a
tomasz.pionka <reports.poland@kinesso.com>
2021-10-01 18:07:50 +0200

    Updated options param

--- flex/scrapper/linux_scrapper.py
+++ flex/scrapper/linux_scrapper.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
-driver = webdriver.Chrome('/home/kinesso/automation/chromedriver', chrome_options=chrome_options,  service_args=['--verbose', '--log-path=/home/kinesso/automation/chromedriver.log'])
+driver = webdriver.Chrome('/home/kinesso/automation/chromedriver', options=chrome_options,  service_args=['--verbose', '--log-path=/home/kinesso/automation/chromedriver.log'])
 driver.get('https://google.org')
 print(driver.title)
+driver.close()
