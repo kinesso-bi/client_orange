d7057980f6a3f5ffe20cf965b2c5f0995e0ecfed
tomasz.pionka <reports.poland@kinesso.com>
2021-10-01 18:02:05 +0200

    Change driver setup

--- flex/scrapper/linux_scrapper.py
+++ flex/scrapper/linux_scrapper.py
@@ -1,19 +1,7 @@
 from selenium import webdriver
 
-# Option 1 - with ChromeOptions
 chrome_options = webdriver.ChromeOptions()
-chrome_options.use_chromium = True
 chrome_options.add_argument('--headless')
-chrome_options.add_argument(
-    '--no-sandbox')  # required when running as root user. otherwise you would get no sandbox errors.
-chrome_options.add_argument('--ignore-certificate-errors')
-# Chrome:
-myProxy = "10.0.x.x:yyyy"
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--proxy-server=%s' % myProxy)
-path = r"/home/kinesso/automation/chromedriver"
-driver = webdriver.Chrome(path, options=chrome_options,
-                          service_args=['--verbose', '--log-path=/home/kinesso/automation/chromedriver.log'])
-
-driver.get('https://python.org')
+driver = webdriver.Chrome('/home/kinesso/automation/chromedriver', chrome_options=chrome_options,  service_args=['--verbose', '--log-path=/tmp/logs/chromedriver.log'])
+driver.get('https://google.org')
 print(driver.title)
