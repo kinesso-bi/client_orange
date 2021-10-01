72387a444b82fbba0f4adb1fbb6629e495cdd021
tomasz.pionka <reports.poland@kinesso.com>
2021-10-01 18:10:39 +0200

    Changed url

--- flex/scrapper/linux_scrapper.py
+++ flex/scrapper/linux_scrapper.py
@@ -3,6 +3,6 @@ from selenium import webdriver
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 driver = webdriver.Chrome('/home/kinesso/automation/chromedriver', options=chrome_options,  service_args=['--verbose', '--log-path=/home/kinesso/automation/chromedriver.log'])
-driver.get('https://google.org')
+driver.get('https://www.python.org/')
 print(driver.title)
 driver.close()
