959141b215a39fe40d949d74a70edae9911ded4c
tomasz.pionka <reports.poland@kinesso.com>
2021-10-01 18:05:15 +0200

    Updated log paths

--- flex/scrapper/linux_scrapper.py
+++ flex/scrapper/linux_scrapper.py
@@ -2,6 +2,6 @@ from selenium import webdriver
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
-driver = webdriver.Chrome('/home/kinesso/automation/chromedriver', chrome_options=chrome_options,  service_args=['--verbose', '--log-path=/tmp/logs/chromedriver.log'])
+driver = webdriver.Chrome('/home/kinesso/automation/chromedriver', chrome_options=chrome_options,  service_args=['--verbose', '--log-path=/home/kinesso/automation/chromedriver.log'])
 driver.get('https://google.org')
 print(driver.title)
