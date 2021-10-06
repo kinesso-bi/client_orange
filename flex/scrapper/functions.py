e1c4c1be281ce947934b1ddf28ca0dc7028f9add
tomasz.pionka <reports.poland@kinesso.com>
2021-10-06 07:25:30 +0200

    Fixed dynamic paths

--- flex/scrapper/functions.py
+++ flex/scrapper/functions.py
@@ -1,11 +1,16 @@
+import json
+import os
+import smtplib
 from google.api_core.exceptions import BadRequest, Conflict, NotFound
 from google.cloud import bigquery
 from google.oauth2 import service_account
-import json
+import datetime
+
 
 def create_client():
     # TODO(developer): Set key_path to the path to the service account key file.
-    key_path = "service_account.json"
+    path = os.path.dirname(os.path.realpath(__file__))
+    key_path = "{}/service_account.json".format(path)
     credentials = service_account.Credentials.from_service_account_file(
         key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
     )
@@ -39,10 +44,62 @@ def upload_data(dataset_name: str, table_name: str, input_data):
     except (BadRequest, Conflict, NotFound) as e:
         print('ERROR: {}'.format(e))
 
+
 def get_credentials():
+    path = os.path.dirname(os.path.realpath(__file__))
     try:
-        with open('credentials.json') as file:
+        with open('{}/credentials.json'.format(path)) as file:
             credentials = json.load(file)
         return credentials
     except FileNotFoundError as f:
         return None
+
+
+def mail_log(get_app_id, get_report_type, error_code, message):
+    path = os.path.dirname(os.path.realpath(__file__))
+    with open('{}/mail.txt'.format(path), 'a+') as file_object:
+        log = '{}, {}, {}, {}, {}'.format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), get_app_id, get_report_type,
+                                          error_code, message)
+        file_object.seek(0)
+        logs = file_object.read(100)
+        if len(logs) > 0:
+            file_object.write('\n')
+            file_object.write(log)
+        else:
+            file_object.write(log)
+
+
+def get_token(app_id, report_type):
+    path = os.path.dirname(os.path.realpath(__file__))
+    try:
+        with open('{}/credentials.json'.format(path)) as file:
+            credentials = json.load(file)
+        return credentials
+    except FileNotFoundError as f:
+        # error_log(app_id, report_type, 1, f)
+        mail_log(app_id, report_type, 1, f)
+        return None
+
+
+def send_mail_log(recipients):
+    path = os.path.dirname(os.path.realpath(__file__))
+    with open('{}/mail.txt'.format(path)) as file_object:
+        lines = file_object.read()
+
+    credentials = get_token(0, 0)
+    gmail_user = credentials["gmail_user"]
+    gmail_password = credentials["gmail_password"]
+    sent_from = gmail_user
+    to = recipients
+    subject = 'Flex - MySQL upload notification'
+    body = lines
+    message = 'Subject: {}\n\nLogs:\n{}'.format(subject, body)
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(sent_from, to, message)
+        server.close()
+        print('Email sent!')
+    except:
+        print('Something went wrong...')
